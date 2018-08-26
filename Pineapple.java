d6a5aceabaf2901e169649a420bcdd698548d665
TheBandiman <42718432+TheBandiman@users.noreply.github.com>
2018-08-26 09:58:26 -0700

    Create Pineapple.java

---
+++ Pineapple.java
@@ -0,0 +1,180 @@
+ import java.util.Random;
+public class Sentence
+{
+  public static void main(String args[])
+  {
+   Random rand = new Random();
+    
+    int x = rand.nextInt(100) + 1;
+    
+    if (1<=x && x<=10)
+    {
+      System.out.print("Rishith is a orangutan.");
+      Random set1 = new Random();
+      
+      int y1 = set1.nextInt(2) + 1;
+      
+      if (y1 == 1)
+      {
+        System.out.print(" His face was burned off in a fire.");
+      }
+      if (y1 == 2)
+      {
+        System.out.print(" He has a carrot stuck up his butt.");
+      }
+    }
+    if (11<=x && x<=20)
+    {
+      System.out.print("Rishith is a Fortnite addict.");
+      
+      Random set2 = new Random();
+      
+      int y2 = set2.nextInt(2) + 1;
+      
+      if (y2 == 1)
+      {
+        System.out.print(" He plays 18 hours a day.");
+      }
+      if (y2 == 2)
+      {
+        System.out.print(" He is complete trash.");
+      }
+    }
+    if (21<=x && x<=30)
+    {
+      System.out.print(" Praneeth is better than Rishith at Fornite.");
+      
+      Random set3 = new Random();
+      
+      int y3 = set3.nextInt(2) + 1;
+      
+      if (y3 == 1)
+      {
+        System.out.println(" This is a fact.");
+      }
+      if (y3 == 2)
+      {
+        System.out.println(" Nope. Just Kidding.");
+      }
+    }
+    if (31<=x && x<=40)
+    {
+      System.out.print(":):):):):):):):):):):):):):)");
+      
+      Random set4 = new Random();
+      
+      int y4 = set4.nextInt(2) + 1;
+      
+      if (y4 == 1)
+      {
+        System.out.println(" I am very happy today.");
+      }
+      if (y4 == 2)
+      {
+        System.out.println(" I am a berry.");
+      }
+    }
+    if (41<=x && x<=50)
+    {
+      System.out.print(":(:(:(:(:(:(:(:(:(:(:(:(:(:(");
+      
+      Random set5 = new Random();
+      
+      int y5 = set5.nextInt(2) + 1;
+      
+      if (y5 == 1)
+      {
+        System.out.println(" I am very sad today.");
+      }
+      if (y5 == 2)
+      {
+        System.out.println(" I am a plump plum.");
+      }
+    }
+    if (51<=x && x<=60)
+    {
+      System.out.print("Let's play PuBG!");
+      
+      Random set6 = new Random();
+      
+      int y6 = set6.nextInt(2) + 1;
+      
+      if (y6 == 1)
+      {
+        System.out.println(" I need to get another win!");
+      }
+      if (y6 == 2)
+      {
+        System.out.println(" Actually, nevermind.");
+      }
+    }
+    if (61<=x && x<=70)
+    {
+      System.out.print("Darza's Dominion is the best game!");
+      
+      Random set7 = new Random();
+      
+      int y7 = set7.nextInt(2) + 1;
+      
+      if (y7 == 1)
+      {
+        System.out.println(" I play all the time!");
+      }
+      if (y7 == 2)
+      {
+        System.out.println(" It's so damn fun! Rishith is just an idiot for not appreciating it.");
+      }
+    }
+    if (71<=x && x<=80)
+    {
+      System.out.print("Eternium is the best game!");
+      
+      Random set8 = new Random();
+      
+      int y8 = set8.nextInt(2) + 1;
+      
+      if (y8 == 1)
+      {
+        System.out.println(" It's even better than Darza's Dominion.");
+      }
+      if (y8 == 2)
+      {
+        System.out.println(" I wish it had multiplayer.");
+      }
+    }
+    if (81<=x && x<=90)
+    {
+      System.out.print("Rishith is a terrible cook.");
+      
+      Random set9 = new Random();
+      
+      int y9 = set9.nextInt(2) + 1;
+      
+      if (y9 == 1)
+      {
+        System.out.println(" Even Praneeth can cook better than him!");
+      }
+      if (y9 == 2)
+      {
+        System.out.println(" He messes up his potatoes every time.");
+      }
+    }
+    if (91<=x && x<=100)
+    {
+      System.out.print("YEEEEEEEEEEEEEEEEEEEET!");
+      
+      Random set10 = new Random();
+      
+      int y10 = set10.nextInt(2) + 1;
+      
+      if (y10 == 1)
+      {
+        System.out.println(" I am almost done with this program.");
+      }
+      if (y10 == 2)
+      {
+        System.out.println(" :|");
+      }
+    }
+  }
+}
